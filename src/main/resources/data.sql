f7fa1a91c5bb4f52e5d4ebbf21d0d2df4fd2ae07
Junhyeok Choi <junwork123@gmail.com>
2022-10-13 23:42:49 +0900

    H2 DB 초기설정 테스트

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--Insert into User values (12865, "평범한 배낭", "https://www.acmicpc.net/problem/12865", "");
+Insert into User values (1, 1234, 'admin', 'james');
+Insert into User values (2, 1234, 'admin', 'jane');
+Insert into User values (3, 1234, 'admin', 'john');
