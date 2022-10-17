6b6bee9c216d0f7f5f3e17d4ba6b971db18b17fc
Junhyeok Choi <junwork123@gmail.com>
2022-10-17 23:50:42 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@
 Insert into User values (1, 1234, 'admin', 'james');
 Insert into User values (2, 1234, 'admin', 'jane');
 Insert into User values (3, 1234, 'admin', 'john');
+
+--서울에 위치한 식당 목록 출력하기
+select * from restaurant where address like '%서울%';
+
