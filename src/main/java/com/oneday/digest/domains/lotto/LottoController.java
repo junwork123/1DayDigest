ced0e741df93a2c1c881ef9408929d1e7dbb6948
Junhyeok Choi <junwork123@gmail.com>
2023-01-14 23:22:26 +0900

    :sparkles: feat: Lotto 서비스 기본 클래스 추가

---
+++ src/main/java/com/oneday/digest/domains/lotto/LottoController.java
@@ -0,0 +1,8 @@
+package com.oneday.digest.domains.lotto;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/lotto")
+public class LottoController {
+
+}
---
+++ src/main/java/com/oneday/digest/domains/lotto/LottoService.java
@@ -0,0 +1,7 @@
+package com.oneday.digest.domains.lotto;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoService {
+}
