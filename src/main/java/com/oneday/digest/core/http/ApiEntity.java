f3ac9ff31c3ec33072708f7c7c79fb79692c2817
Junhyeok Choi <junwork123@gmail.com>
2023-01-16 22:49:10 +0900

    :art: refactor: ApiEntity를 EntityBase를 상속하도록 변경

--- src/main/java/com/oneday/digest/core/http/ApiEntity.java
+++ src/main/java/com/oneday/digest/core/http/ApiEntity.java
@@ -1,28 +1,19 @@
 package com.oneday.digest.core.http;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import com.oneday.digest.core.entity.EntityBase;
 import org.springframework.http.HttpStatus;
 
-public class ApiEntity {
+public class ApiEntity extends EntityBase {
     public static <T> ApiResult<T> success(T response) {
         return new ApiResult<>(true, response, null);
     }
-    public static ApiResult<?> error(HttpStatus status, String message) {
-        return new ApiResult<>(false, null, new ApiError(status, message));
+    public static ApiResult error(HttpStatus status, String message) {
+        return new ApiResult<>(false, "", new ApiError(status, message));
     }
 
     public record ApiError(HttpStatus status, String message) {
-        @Override
-        public String toString() {
-            return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
-        }
     }
 
     public record ApiResult<T>(boolean success, T response, ApiError error) {
-        @Override
-        public String toString() {
-            return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
-        }
     }
 }
