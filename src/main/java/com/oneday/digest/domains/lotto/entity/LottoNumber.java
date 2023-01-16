8b0403cdd6d8154514dc4d72bc91f32ef4a4ae12
Junhyeok Choi <junwork123@gmail.com>
2023-01-16 22:48:01 +0900

    :truck: refactor: Record 변수명 변경

--- src/main/java/com/oneday/digest/domains/lotto/entity/LottoNumber.java
+++ src/main/java/com/oneday/digest/domains/lotto/entity/LottoNumber.java
@@ -2,16 +2,16 @@ package com.oneday.digest.domains.lotto.entity;
 
 import java.util.Objects;
 
-public record LottoNumber(int numbers) {
+public record LottoNumber(int value) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof LottoNumber that)) return false;
-        return numbers == that.numbers;
+        return value == that.value;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(numbers);
+        return Objects.hash(value);
     }
 }
