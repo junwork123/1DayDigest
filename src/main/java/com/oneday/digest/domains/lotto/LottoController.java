95632d676dad3bc11d04b6824bfb40e379c649f3
Junhyeok Choi <junwork123@gmail.com>
2023-01-18 23:21:16 +0900

    :fire: revert: DTO 관련 코드는 추후 작성하도록 revert

--- src/main/java/com/oneday/digest/domains/lotto/LottoController.java
+++
@@ -1,8 +0,0 @@
-package com.oneday.digest.domains.lotto;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/api/v1/lotto")
-public class LottoController {
-
-}
--- src/main/java/com/oneday/digest/domains/lotto/LottoService.java
+++
@@ -1,7 +0,0 @@
-package com.oneday.digest.domains.lotto;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class LottoService {
-}
