fd0884054a0f4e67cd946d924cf3bc383d6dd001
Junhyeok Choi <junwork123@gmail.com>
2023-01-26 22:14:31 +0900

    style: Bean Validation 테스트 작성

--- src/test/java/com/oneday/digest/domains/product/ProductControllerTest.java
+++ src/test/java/com/oneday/digest/domains/product/ProductControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,6 +20,9 @@ class ProductControllerTest {
 
     @Autowired
     private Gson gson;
+
+    @MockBean
+    private ProductService productService;
 
     @DisplayName("DTO Validation 실패 테스트")
     @Test
