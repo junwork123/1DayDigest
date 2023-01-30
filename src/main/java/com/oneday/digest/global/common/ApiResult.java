24782cd18590148b9c0b7c7efd8d53c4e4cad86a
Junhyeok Choi <junwork123@gmail.com>
2023-01-30 21:53:43 +0900

    feat: Binding Error 표현을 위한 fail 메소드 추가

--- src/main/java/com/oneday/digest/global/common/ApiResult.java
+++ src/main/java/com/oneday/digest/global/common/ApiResult.java
@@ -1,6 +1,8 @@
 package com.oneday.digest.global.common;
 
 import com.oneday.digest.global.error.ErrorResponse;
+import com.oneday.digest.global.error.exception.ErrorCode;
+import org.springframework.validation.BindingResult;
 
 public class ApiResult {
     public static <T> ApiEntity<T> success(T response) {
@@ -8,6 +10,10 @@ public class ApiResult {
     }
     public static ApiEntity<?> error(ErrorResponse errorResponse) {
         return new ApiEntity<>(false, "", errorResponse);
+    }
+
+    public static ApiEntity<?> fail(BindingResult bindingResult) {
+        return new ApiEntity<>(false, "", ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, bindingResult));
     }
 
     public record ApiEntity<T>(boolean isSuccess, T response, ErrorResponse errorResponse) {
