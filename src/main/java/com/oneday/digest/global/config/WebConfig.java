9c7e254cfbad385628929d538b4f01b70138e634
Junhyeok Choi <junwork123@gmail.com>
2023-02-05 00:25:33 +0900

    feat: 로그인 유저를 확인하는 Filter 추가

--- src/main/java/com/oneday/digest/global/config/WebConfig.java
+++ src/main/java/com/oneday/digest/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.oneday.digest.global.config;
 
 import com.oneday.digest.global.config.filter.LogFilter;
+import com.oneday.digest.global.config.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,18 @@ public class WebConfig {
         filterRegistrationBean.setFilter((Filter) new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new
+                FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter((Filter) new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+
         return filterRegistrationBean;
     }
 }
